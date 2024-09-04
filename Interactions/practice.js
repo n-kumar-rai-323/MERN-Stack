090b73f34334e857a63be4851b84d0ccc0daff66
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-04 22:29:22 +0545

    MERN

--- Interactions/practice.js
+++ Interactions/practice.js
@@ -1,5 +1,5 @@
 const numb = prompt("Enter First Number :");
 const numb2 = prompt("Enter Second Number :");
-const sum = numb + numb2;
+const sum = Number(numb) + Number(numb2);
 console.log({ numb, numb2 });
 alert(`the ${numb} and ${numb2} sum is  ${sum}`);
---
+++ Statement/script.js
@@ -0,0 +1,19 @@
+//Statement
+
+// if else
+// switch case
+
+// Syntax
+// if(condition){
+//     logic
+// }else{
+//     logic
+// }
+
+const gender = prompt("Are you Mail");
+
+if (gender == "Yes") {
+  alert("You are Male");
+} else {
+  alert("You are Female");
+}
