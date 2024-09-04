4ec8ad188a2947353ab8ff8da60d4f9f42fbfea9
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-04 22:05:15 +0545

    MERN

---
+++ Interactions/practice.js
@@ -0,0 +1,5 @@
+const numb = prompt("Enter First Number :");
+const numb2 = prompt("Enter Second Number :");
+const sum = numb + numb2;
+console.log({ numb, numb2 });
+alert(`the ${numb} and ${numb2} sum is  ${sum}`);
