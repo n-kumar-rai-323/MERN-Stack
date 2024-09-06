86565a873c9157dc31443615b93b4ebfce7363c4
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-06 22:42:15 +0545

    MERN

--- Ad_Function/function.js
+++ Ad_Function/function.js
@@ -32,11 +32,43 @@ console.log(count1());
 const count2 = closerFunc();
 console.log(count2());
 console.log(count2());
-// 5. IIFes
+// 5. IIFes (Immediately Invoked Functional Expression)
+((name) => {
+  console.log(`${name}`);
+})("Nishan");
 // 6. recursive function
+const factorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return factorial(num - 1);
+};
+
+const fact = factorial(5);
+console.log(fact);
 // 7. inline function
 // 8. anonymous function
 // 9. callback function
+const print = (data) => {
+  console.log({ data });
+};
+
+const parent = (func) => {
+  const num = "Nishan";
+  func(num);
+};
+parent(print);
 // 10. pure function
+
+const uper = (text) => {
+  console.log(text.toUpperCase());
+};
+
+uper("nishan rai");
 // 11. implicit function
+const summ = (a, b) => a + b;
+
 // 12. explicit  function
+const summa = (a, b) => {
+  return a + b;
+};
