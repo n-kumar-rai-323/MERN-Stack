cffbcef64d1ad6c37fef35be056fa58d78b40c24
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-06 20:56:51 +0545

    MERN

--- Ad_Function/function.js
+++ Ad_Function/function.js
@@ -1,14 +1,23 @@
-// // type of function 
-// 1. default function 
+// // type of function
+// 1. default function
+
+const defaultFunc = (name = "user") => {
+  console.log(`${name}`);
+};
+defaultFunc("Nishan");
+defaultFunc();
 // 2. parameterized function
+const secondFunc=({a,b,c,d})=>{
+    console.log(a + b)
+}
+secondFunc({a:2,c:4,d:3,b:3})
 // 3. arrow function
-// 4. closure 
-// 5. IIFes 
+// 4. closure
+// 5. IIFes
 // 6. recursive function
 // 7. inline function
 // 8. anonymous function
 // 9. callback function
 // 10. pure function
 // 11. implicit function
-// 12. explicit  function 
-
+// 12. explicit  function
