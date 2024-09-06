dc4d677e86af99fc149450bf11210092addd0d1f
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-06 20:28:12 +0545

    MERN

---
+++ Ad_Function/function.js
@@ -0,0 +1,14 @@
+// // type of function 
+// 1. default function 
+// 2. parameterized function
+// 3. arrow function
+// 4. closure 
+// 5. IIFes 
+// 6. recursive function
+// 7. inline function
+// 8. anonymous function
+// 9. callback function
+// 10. pure function
+// 11. implicit function
+// 12. explicit  function 
+
