754379cb5391ff3c913f04980a29a0f4c2efb605
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-06 21:16:54 +0545

    MERN

--- Ad_Function/function.js
+++ Ad_Function/function.js
@@ -6,13 +6,32 @@ const defaultFunc = (name = "user") => {
 };
 defaultFunc("Nishan");
 defaultFunc();
+
 // 2. parameterized function
-const secondFunc=({a,b,c,d})=>{
-    console.log(a + b)
-}
-secondFunc({a:2,c:4,d:3,b:3})
+const secondFunc = ({ a, b, c, d }) => {
+  console.log(a + b);
+};
+secondFunc({ a: 2, c: 4, d: 3, b: 3 });
+
 // 3. arrow function
+const arroFunc = (a, b) => {
+  return a + b;
+};
 // 4. closure
+const closerFunc = () => {
+  let count = 0;
+  return () => {
+    count = count + 1;
+    return count;
+  };
+};
+const count1 = closerFunc();
+console.log(count1());
+console.log(count1());
+
+const count2 = closerFunc();
+console.log(count2());
+console.log(count2());
 // 5. IIFes
 // 6. recursive function
 // 7. inline function
