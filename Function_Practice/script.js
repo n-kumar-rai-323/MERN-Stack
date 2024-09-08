1dedf9e7e6c0020e7a092734a08617a702bbbf84
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-08 14:10:13 +0545

    MERN

--- Function_Practice/script.js
+++ Function_Practice/script.js
@@ -17,3 +17,24 @@ console.log(reverseNumbera(number));
 
 const reverseNumber = (num) => String(num).split("").reverse().join("");
 console.log(reverseNumber(123456789));
+
+// write a javascript function that accepts a string as a parameter and counts the number of vowels
+const vowels = (sentence) => {
+  let count = 0;
+  const text = sentence.toLowerCase();
+  for (let i = 1; i <= text.length - 1; i++) {
+    if (
+      text[i] === "a" ||
+      text[i] === "e" ||
+      text[i] === "i" ||
+      text[i] === "o" ||
+      text[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const count1 = vowels("My name is Nishan");
+console.log(count1);
