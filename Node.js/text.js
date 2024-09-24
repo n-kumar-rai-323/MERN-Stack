44158f2c4b6b16655b90a772816b7440189ee6fb
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-24 16:12:04 +0545

    MERN

---
+++ Node.js/text.js
@@ -0,0 +1,15 @@
+const lowerCase = (text) => text.toLowerCase();
+
+const upperCase = (text) => text.toUpperCase();
+
+const properCase = (text) =>
+  text
+    .split(" ")
+    .map(
+      (word) =>
+        word.slice(0, 1).toUpperCase() +
+        word.slice(1, word.length).toLowerCase()
+    )
+    .join(" ");
+
+console.log(properCase("nishan KUMAR"));
