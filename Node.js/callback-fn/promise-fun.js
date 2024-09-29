7dd70618efa8fe6ad94e19b94dd67d48441577a1
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-09-29 22:14:57 +0545

    MERN

--- Node.js/callback-fn/promise-fun.js
+++ Node.js/callback-fn/promise-fun.js
@@ -13,3 +13,19 @@ test
   .catch((err) => {
     console.log(err);
   });
+
+//write a promise function that sum 2 number passed to promise function
+const sum = new Promise(a, b, (resolve, reject) => {
+  setTimeout(() => {
+    resolve(a + b);
+  }, 2000);
+});
+
+console.log(sum(4, 5));
+// .then((result) => {
+//   console.log(`sum is ${result}`);
+// })
+
+// .catch((err) => {
+//   console.log(err);
+// });
