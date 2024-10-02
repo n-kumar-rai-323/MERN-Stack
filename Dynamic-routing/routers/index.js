5093eec23238e2b562cb5743f650e82580e729ac
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-02 09:52:45 +0545

    MERN

--- Dynamic-routing/routers/index.js
+++ Dynamic-routing/routers/index.js
@@ -4,4 +4,13 @@ router.get("/", (req, res) => {
   res.json({ data: "Hello World" });
 });
 
+router.get("/:id", (req, res) => {
+  //   console.log(req.params);
+  console.log(req?.query);
+  res.json({
+    data: `Hello id number ${req.params.id} with query data ${JSON.stringify(
+      req.query
+    )}`,
+  });
+});
 module.exports = router;
