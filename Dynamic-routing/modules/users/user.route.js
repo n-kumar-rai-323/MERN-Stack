fa0fd3182fce3ee346e7acac3f089a049ba18865
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-04 05:53:23 +0100

    MERN

--- Dynamic-routing/modules/users/user.route.js
+++ Dynamic-routing/modules/users/user.route.js
@@ -1,70 +1,88 @@
 const router = require("express").Router();
-const multer  = require('multer')
+const multer = require('multer')
 
-const upload = multer({ dest: 'public/uploads/' })
 
-const verify =(req,res,next)=>{
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "." + file.originalname.split(".")[1])
+    }
+})
+
+const upload = multer({ storage })
+// const upload = multer({ dest: 'public/uploads/' })
+
+const verify = (req, res, next) => {
     const role = req.headers.role;
-    if(role === "admin"){
+
+
+    if (role === "admin") {
         next();
-    }else{
+    } else {
         throw new Error("You are not authorize")
     }
 };
-router.post("/register", upload.single('profilePic'),(req,res,next)=>{
-    try{
-        console.log({pic: req.file})
-        res.json({data : "User Registered Successcully"})
-    }catch(e){
+
+
+router.post("/register", upload.single('profilePic'), (req, res, next) => {
+    try {
+        console.log({ pic: req.file })
+        const URL = "http://localhost:7000/resources/uploads/";
+        const myPicture = URL + req?.file?.filename
+        res.json({ data: `User Registered Successcully${myPicture}` })
+    } catch (e) {
         next(e)
     }
 })
-router.get("/",verify,(req,res,next)=>{
+router.get("/", verify, (req, res, next) => {
     //query
-   
-    try{
-        let randomNumber = Math.floor(Math.random()*10)+1
-        if(randomNumber > 5){
-            res.json({data: `Random Number is ${randomNumber}`})
-        }else{
-           throw new Error(`${randomNumber}`)
+
+    try {
+        let randomNumber = Math.floor(Math.random() * 10) + 1
+        if (randomNumber > 5) {
+            res.json({ data: `Random Number is ${randomNumber}` })
+        } else {
+            throw new Error(`${randomNumber}`)
         }
-        
+
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 });
 
 // read one data
-router.get("/:id",(req,res)=>{
+router.get("/:id", (req, res) => {
     //query
-    res.json({data: `get data from user is ${req?.params?.id}`})
+    res.json({ data: `get data from user is ${req?.params?.id}` })
 });
 
 //Create
-router.post("/",(req,res)=>{
+router.post("/", (req, res) => {
     //body
     console.log(req.body)
-    res.json({data: "Hello from post users"})
+    res.json({ data: "Hello from post users" })
 });
 
 //Update
-router.put("/:id",(req,res)=>{
+router.put("/:id", (req, res) => {
     //param + body 
-    res.json({data: "Hello from put users"})
+    res.json({ data: "Hello from put users" })
 });
 
 //Update
-router.patch("/:id",(req,res)=>{
+router.patch("/:id", (req, res) => {
     // params + body 
-    res.json({data: "Hello from patch users"})
+    res.json({ data: "Hello from patch users" })
 });
 
 //Delete
-router.delete("/:id",(req,res)=>{
+router.delete("/:id", (req, res) => {
     //params
-    res.json({data: "Hello from deleteusers"})
+    res.json({ data: "Hello from deleteusers" })
 });
 
-module.exports =router;
+module.exports = router;
