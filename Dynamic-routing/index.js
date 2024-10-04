ad7cc22b15f02dbdc4e6001d8791b0038f30b3fa
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-04 10:17:00 +0100

    MERN

--- Dynamic-routing/index.js
+++ Dynamic-routing/index.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require('morgan')
+const mongoose = require("mongoose");
 const app=express()
 
 const PORT = Number(process.env.PORT) || 8000
 const indexRoute = require("./routers")
 
+
+mongoose.connect("mongodb://localhost:27017/hotel-mgmt").then(()=>{
+    console.log("Database is connected");
+})
+.catch((e)=>{
+    console.log("Database Error");
+})
+
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use("/resources", express.static("public"))
+app.use("/resources", express.static("public"));
 
 
 app.use((req,res,next)=>{
