efd0b66da878f440825cff698509d889ef3ed6d7
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-09 08:40:26 +0100

    MERN

--- Admin user/modules/users/user.module.js
+++ Admin user/modules/users/user.module.js
@@ -5,7 +5,7 @@ const {Schema, model }= require('mongoose');
 const userSchema = new  Schema({
     name : { type: String, required : true},
     password : {type: String, required:true},
-    email : {type: String, required :true},
+    email : {type: String, required :true, unique:true},
     roles : [String], default:["user"], enum:["admin", "user"],
     isActive : {type:Boolean, required:true, default:true},
     image: String
