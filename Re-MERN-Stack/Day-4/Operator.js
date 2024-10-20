e39fc1e980951efdb880d2055394ec9feff0300a
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-20 07:25:03 +0100

    Day 4

---
+++ Re-MERN-Stack/Day-4/Operator.js
@@ -0,0 +1,24 @@
+// Operator 
+// AND
+// OR
+// NOT
+
+// 1. And (both condtion must be true)
+
+// conditon && conditon2 
+// true && true
+
+
+// 2. OR (any 1 conditon must satisfy)
+
+
+// 3. NOT (opposite value)
+const isAdult = true
+console.log(!isAdult)
+
+
+if(!isAdult){
+    console.log("you are watching cartooon");
+}else{
+    console.log("you are watching news")
+}
---
+++ Re-MERN-Stack/Day-4/ternary.js
@@ -0,0 +1,3 @@
+const gender =String(prompt("Enter who are you M or F"))
+
+gender ==="m"? console.log("Male") : console.log("Female")
