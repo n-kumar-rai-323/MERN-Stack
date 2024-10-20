d6eff5e13ea662af43b18c21b4a918c6a7ae5a4e
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-10-20 06:50:17 +0100

    MERN

---
+++ Re-MERN-Stack/Day-4/statement.js
@@ -0,0 +1,24 @@
+const gender = confirm("Are you Male");
+
+if(gender){
+    alert("you are male");
+}else{
+    alert("You are female");
+}
+
+const data = confirm("enter m for male and f for female")
+
+if(data === "m"){
+alert("you are male");
+}else{
+    alert("you are female");
+}
+
+
+// = variable assign 
+// == data type  value compare 
+// === data type value compare + conversion to Boolean
+
+console.log(1 == '1')
+console.log(1==='1')
+console.log(1 === 1)
