85a6ab281113dc044780a4b4235774273b007325
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-18 03:14:36 +0000

    Bootstrap

--- Re-MERN-Stack/fetch/index.js
+++ Re-MERN-Stack/fetch/index.js
@@ -1,9 +1,15 @@
-const fetchData=()=>{
-    try{
-        fetch("https://dummyjson.com/user")
-        .then((result)=>result.json())
-        .then((data)=> console.log({data}));
-    }catch(e){
+const fetchData = async() => {
+    try {
+        const result = await fetch("https://dummyjson.com/user")
+        const data = await result.json();
+        const {users=[]}= data;
+        const firstNames = users.map((user)=>
+        user.firstName.concat(" ", user.lastName)
+        );
+
+        let newHTML = "";
+        newHTML += "<ol>";
+    } catch (e) {
         console.error;
     }
 }
