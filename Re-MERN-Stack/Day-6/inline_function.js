52d1b09940049de5c7d60a367b791d992d9dbf87
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-20 15:14:16 +0000

    Inline function

---
+++ Re-MERN-Stack/Day-6/inline_function.js
@@ -0,0 +1,12 @@
+let d="Rai";
+const test =()=>{
+    const d =1;
+    console.log({d})
+}
+test()
+
+// In ES6 (ECMAScript 2015), inline functions are typically associated with arrow functions,
+//  which offer a concise and modern way to define functions directly in the context where 
+//  they are used. Arrow functions inherently simplify the syntax for inline functions and 
+//  are widely used for callbacks, one-liners, and anonymous function expressions.
+
