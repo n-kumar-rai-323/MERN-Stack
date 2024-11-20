9cfcca053ef0d8f9f3f995f85481f7e6030809d6
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-20 16:09:36 +0000

    Day 6

--- Re-MERN-Stack/Day-6/IIFEs_fun.js
+++ Re-MERN-Stack/Day-6/IIFEs_fun.js
@@ -2,3 +2,4 @@
     const apiKey = "123456";
     console.log("API Key is private and cannot be accessed globally");
 })();
+// Immediately Invoked Function Expressions (IIFEs) in JavaScript are functions that are executed as soon as they are defined. They are a powerful tool for managing scope and avoiding variable pollution, especially in real-world scenarios.
