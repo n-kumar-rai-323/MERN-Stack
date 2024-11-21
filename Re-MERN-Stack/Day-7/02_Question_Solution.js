f417bdf3e5496d3ec70c61030f0ae9364fe26300
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-11-21 11:19:30 +0000

    Question_Solution

---
+++ Re-MERN-Stack/Day-7/02_Question_Solution.js
@@ -0,0 +1,17 @@
+// write a JavaScript function that accepts a string as a 
+// parameter and counts the number of vowels within the string. 
+// Note: As the letter 'Y' can be regarded as both a vowel and a consonant, we do not count 'y' as vowel here. Example string: : The quick brown fox' expected output : 5
+
+
+const checVowel = (iphone) => {
+    let countVowel = 0;
+    const text = iphone.toLowerCase();
+    for (let i = 0; i <= text.length - 1; i++) {
+        if (text[i] === "a" ||  text[i] == "e" || text[i] === "i" || text[i] === "o" || text[i] === "u") {
+            countVowel++;
+        }
+    }
+    return countVowel;
+}
+const count = checVowel("The quick brown fox");
+console.log(count)
